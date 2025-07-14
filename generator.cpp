ba9d1be86fee08a52ace79607b7d09537a413f68
Simon Spannagel <simon.spannagel@cern.ch>
2025-07-14 10:36:22 +0200

    Add build file, main file and generator

---
+++ generator.cpp
@@ -0,0 +1,27 @@
+// Copyright (c) 2025 DESY and the Constellation authors
+// SPDX-License-Identifier: EUPL-1.2 OR CC0-1.0
+
+#include <memory>
+#include <string_view>
+
+#include <constellation/build.hpp>
+#include <constellation/satellite/Satellite.hpp>
+
+#include "TemplateSatellite.hpp"
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wreturn-type-c-linkage"
+#endif
+
+// generator function for loading satellite from shared library
+extern "C" {
+CNSTLN_DLL_EXPORT
+std::shared_ptr<constellation::satellite::Satellite> generator(std::string_view type, std::string_view name) {
+    return std::make_shared<TemplateSatellite>(type, name);
+}
+}
+
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
